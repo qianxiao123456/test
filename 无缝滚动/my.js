376bff9886912c67cc37cc794a1f8a3537997542
qianxiao123456 <3211949396@qq.com>
2019-03-29 15:28:15 +0800

    picture

---
+++ 无缝滚动/my.js
@@ -0,0 +1,17 @@
+// window.onload = function () {   
+//     var speed = 2;
+//     var finn;
+//     var center = document.querySelector(".center");
+//     // var center = document.getElementsByClassName("center")[0];
+//      finn = parseInt(center.style.marginLeft) ;
+//     function fun(){
+//         if(finn == -1000 ){
+//             finn = 0;
+//         }
+//         else{
+//             finn -= speed;   
+//         }   
+//         center.style.marginLeft = finn+ "px";
+//     }
+//     var times = setInterval(fun,10);                     
+// }
